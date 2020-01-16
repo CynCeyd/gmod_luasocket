105d34b19669910b4c7acc99b2e608a6d21c9407
Daniel <danielga@outlook.pt>
2020-01-16 01:16:58 +0000

    Add whitelist code
    Add luasocket headers to projects
    Cleanup premake code
    Changed luasocket submodule for personal fork

--- premake5.lua
+++ premake5.lua
@@ -8,13 +8,22 @@ local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(path.join(gmcommon, "generator.v2.lua"))
 
+newoption({
+	trigger = "enable-whitelist",
+	description = "Enables a whitelist file to filter valid addresses and ports"
+})
+
 local LUASOCKET_FOLDER = "luasocket/src"
 
 CreateWorkspace({name = "socket.core"})
 	CreateProject({serverside = true, manual_files = true})
 		files("source/socket.cpp")
 		IncludeLuaShared()
+		includedirs(LUASOCKET_FOLDER)
 		links("socket")
+
+		filter("options:enable-whitelist")
+			defines("ENABLE_WHITELIST")
 
 		filter("system:windows")
 			links("ws2_32")
@@ -22,47 +31,72 @@ CreateWorkspace({name = "socket.core"})
 	CreateProject({serverside = false, manual_files = true})
 		files("source/socket.cpp")
 		IncludeLuaShared()
+		includedirs(LUASOCKET_FOLDER)
 		links("socket")
+
+		filter("options:enable-whitelist")
+			defines("ENABLE_WHITELIST")
 
 		filter("system:windows")
 			links("ws2_32")
 
 	project("socket")
 		kind("StaticLib")
-		warnings("Default")
 		includedirs(LUASOCKET_FOLDER)
 		files({
 			LUASOCKET_FOLDER .. "/auxiliar.c",
+			LUASOCKET_FOLDER .. "/auxiliar.h",
 			LUASOCKET_FOLDER .. "/buffer.c",
+			LUASOCKET_FOLDER .. "/buffer.h",
 			LUASOCKET_FOLDER .. "/compat.c",
+			LUASOCKET_FOLDER .. "/compat.h",
 			LUASOCKET_FOLDER .. "/except.c",
+			LUASOCKET_FOLDER .. "/except.h",
+			LUASOCKET_FOLDER .. "/filter.h",
 			LUASOCKET_FOLDER .. "/inet.c",
+			LUASOCKET_FOLDER .. "/inet.h",
 			LUASOCKET_FOLDER .. "/io.c",
+			LUASOCKET_FOLDER .. "/io.h",
 			LUASOCKET_FOLDER .. "/luasocket.c",
+			LUASOCKET_FOLDER .. "/luasocket.h",
 			LUASOCKET_FOLDER .. "/options.c",
+			LUASOCKET_FOLDER .. "/options.h",
+			LUASOCKET_FOLDER .. "/pierror.h",
 			LUASOCKET_FOLDER .. "/select.c",
+			LUASOCKET_FOLDER .. "/select.h",
 			LUASOCKET_FOLDER .. "/tcp.c",
+			LUASOCKET_FOLDER .. "/tcp.h",
 			LUASOCKET_FOLDER .. "/timeout.c",
-			LUASOCKET_FOLDER .. "/udp.c"
+			LUASOCKET_FOLDER .. "/timeout.h",
+			LUASOCKET_FOLDER .. "/udp.c",
+			LUASOCKET_FOLDER .. "/udp.h"
 		})
-		vpaths({["Source files/*"] = LUASOCKET_FOLDER .. "/*.c"})
+		vpaths({
+			["Source files/*"] = LUASOCKET_FOLDER .. "/*.c",
+			["Header files/*"] = LUASOCKET_FOLDER .. "/*.h"
+		})
 		IncludeLuaShared()
+
+		filter("options:enable-whitelist")
+			defines("LUASOCKET_ENABLE_WHITELIST")
 
 		filter("system:windows")
 			defines({
 				"LUASOCKET_API=__declspec(dllexport)",
-				"MIME_API=__declspec(dllexport)"
+				"_WINSOCK_DEPRECATED_NO_WARNINGS"
 			})
-			files(LUASOCKET_FOLDER .. "/wsocket.c")
+			files({
+				LUASOCKET_FOLDER .. "/wsocket.c",
+				LUASOCKET_FOLDER .. "/wsocket.h"
+			})
 			links("ws2_32")
 
 		filter("system:not windows")
-			defines({
-				"LUASOCKET_API=''",
-				"UNIX_API=''",
-				"MIME_API=''"
+			defines("LUASOCKET_API=''")
+			files({
+				LUASOCKET_FOLDER .. "/usocket.c",
+				LUASOCKET_FOLDER .. "/usocket.h"
 			})
-			files(LUASOCKET_FOLDER .. "/usocket.c")
 
 CreateWorkspace({name = "mime.core"})
 	CreateProject({serverside = true, manual_files = true})
@@ -77,27 +111,24 @@ CreateWorkspace({name = "mime.core"})
 
 	project("mime")
 		kind("StaticLib")
-		warnings("Default")
 		includedirs(LUASOCKET_FOLDER)
 		files({
 			LUASOCKET_FOLDER .. "/compat.c",
-			LUASOCKET_FOLDER .. "/mime.c"
+			LUASOCKET_FOLDER .. "/compat.h",
+			LUASOCKET_FOLDER .. "/mime.c",
+			LUASOCKET_FOLDER .. "/mime.h"
 		})
-		vpaths({["Source files/*"] = LUASOCKET_FOLDER .. "/*.c"})
+		vpaths({
+			["Source files/*"] = LUASOCKET_FOLDER .. "/*.c",
+			["Header files/*"] = LUASOCKET_FOLDER .. "/*.h"
+		})
 		IncludeLuaShared()
 
 		filter("system:windows")
-			defines({
-				"LUASOCKET_API=__declspec(dllexport)",
-				"MIME_API=__declspec(dllexport)"
-			})
+			defines("LUASOCKET_API=__declspec(dllexport)")
 
 		filter("system:not windows")
-			defines({
-				"LUASOCKET_API=''",
-				"UNIX_API=''",
-				"MIME_API=''"
-			})
+			defines("LUASOCKET_API=''")
 
 if os.istarget("linux") or os.istarget("macosx") then
 	CreateWorkspace({name = "socket.unix"})
@@ -113,24 +144,35 @@ if os.istarget("linux") or os.istarget("macosx") then
 
 		project("unix")
 			kind("StaticLib")
-			warnings("Default")
-			defines({
-				"LUASOCKET_API=''",
-				"UNIX_API=''",
-				"MIME_API=''"
-			})
+			defines("LUASOCKET_API=''")
 			includedirs(LUASOCKET_FOLDER)
 			files({
 				LUASOCKET_FOLDER .. "/auxiliar.c",
+				LUASOCKET_FOLDER .. "/auxiliar.h",
 				LUASOCKET_FOLDER .. "/buffer.c",
+				LUASOCKET_FOLDER .. "/buffer.h",
 				LUASOCKET_FOLDER .. "/compat.c",
+				LUASOCKET_FOLDER .. "/compat.h",
 				LUASOCKET_FOLDER .. "/io.c",
+				LUASOCKET_FOLDER .. "/io.h",
 				LUASOCKET_FOLDER .. "/options.c",
+				LUASOCKET_FOLDER .. "/options.h",
+				LUASOCKET_FOLDER .. "/pierror.h",
 				LUASOCKET_FOLDER .. "/timeout.c",
+				LUASOCKET_FOLDER .. "/timeout.h",
 				LUASOCKET_FOLDER .. "/unix.c",
-				LUASOCKET_FOLDER .. "/usocket.c"
+				LUASOCKET_FOLDER .. "/unix.h",
+				LUASOCKET_FOLDER .. "/unixdgram.c",
+				LUASOCKET_FOLDER .. "/unixdgram.h",
+				LUASOCKET_FOLDER .. "/unixstream.c",
+				LUASOCKET_FOLDER .. "/unixstream.h",
+				LUASOCKET_FOLDER .. "/usocket.c",
+				LUASOCKET_FOLDER .. "/usocket.h"
 			})
-			vpaths({["Source files/*"] = LUASOCKET_FOLDER .. "/*.c"})
+			vpaths({
+				["Source files/*"] = LUASOCKET_FOLDER .. "/*.c",
+				["Header files/*"] = LUASOCKET_FOLDER .. "/*.h"
+			})
 			IncludeLuaShared()
 
 	CreateWorkspace({name = "socket.serial"})
@@ -146,23 +188,29 @@ if os.istarget("linux") or os.istarget("macosx") then
 
 		project("serial")
 			kind("StaticLib")
-			warnings("Default")
-			defines({
-				"LUASOCKET_API=''",
-				"UNIX_API=''",
-				"MIME_API=''"
-			})
+			defines("LUASOCKET_API=''")
 			includedirs(LUASOCKET_FOLDER .. "/src")
 			files({
 				LUASOCKET_FOLDER .. "/auxiliar.c",
+				LUASOCKET_FOLDER .. "/auxiliar.h",
 				LUASOCKET_FOLDER .. "/buffer.c",
+				LUASOCKET_FOLDER .. "/buffer.h",
 				LUASOCKET_FOLDER .. "/compat.c",
+				LUASOCKET_FOLDER .. "/compat.h",
 				LUASOCKET_FOLDER .. "/io.c",
+				LUASOCKET_FOLDER .. "/io.h",
 				LUASOCKET_FOLDER .. "/options.c",
+				LUASOCKET_FOLDER .. "/options.h",
+				LUASOCKET_FOLDER .. "/pierror.h",
 				LUASOCKET_FOLDER .. "/timeout.c",
+				LUASOCKET_FOLDER .. "/timeout.h",
 				LUASOCKET_FOLDER .. "/usocket.c",
+				LUASOCKET_FOLDER .. "/usocket.h",
 				LUASOCKET_FOLDER .. "/serial.c"
 			})
-			vpaths({["Source files/*"] = LUASOCKET_FOLDER .. "/*.c"})
+			vpaths({
+				["Source files/*"] = LUASOCKET_FOLDER .. "/*.c",
+				["Header files/*"] = LUASOCKET_FOLDER .. "/*.h"
+			})
 			IncludeLuaShared()
 end
